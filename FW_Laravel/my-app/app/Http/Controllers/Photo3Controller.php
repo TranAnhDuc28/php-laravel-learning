97608256d15a1756865a95ae9cb8ae8ebfbac496
Trần Anh Đức <ducanhtran28112x@gmail.com>
2025-03-01 14:02:20 +0700

    update project my-app

---
+++ FW_Laravel/my-app/app/Http/Controllers/Photo3Controller.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Photo2;
+use App\Http\Requests\StorePhoto2Request;
+use App\Http\Requests\UpdatePhoto2Request;
+
+class Photo3Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePhoto2Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Photo2 $photo2)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Photo2 $photo2)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePhoto2Request $request, Photo2 $photo2)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Photo2 $photo2)
+    {
+        //
+    }
+}
